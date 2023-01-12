1a06bde04bbfa1ef54032c2eae17240d8e0c560c
FranJMM <francisco.martinezmencias@telefonica.com>
2023-01-12 11:45:35 +0100

    Create Unit Tests for usecases module

---
+++ usecases/src/test/java/com/martinezmencias/eventscheduler/usecases/RequestEventsUseCaseTest.kt
@@ -0,0 +1,30 @@
+package com.martinezmencias.eventscheduler.usecases
+
+import com.martinezmencias.eventscheduler.data.repository.EventRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+
+@RunWith(MockitoJUnitRunner::class)
+class RequestEventsUseCaseTest {
+
+    @Mock
+    lateinit var eventRepository: EventRepository
+
+    private lateinit var requestEventsUseCase: RequestEventsUseCase
+
+    @Before
+    fun setUp() {
+        requestEventsUseCase = RequestEventsUseCase(eventRepository)
+    }
+
+    @Test
+    fun `Invoke calls event repository`(): Unit = runBlocking {
+        requestEventsUseCase()
+        verify(eventRepository).requestEvents()
+    }
+}
